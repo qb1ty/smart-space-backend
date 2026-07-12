ad61c55ef89b4cfaee8a77ef255b7aea9be3050d
qb1ty <arsenbro0707@mail.ru>
2026-07-12 21:34:06 +0500

    add: добавл dto для создания space

--- src/spaces/dto/create-space.dto.ts
+++ src/spaces/dto/create-space.dto.ts
@@ -1,1 +1,33 @@
-export class CreateSpaceDto {}
+import { SpaceType } from "@prisma/client";
+import { IsBoolean, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateSpaceDto {
+    @IsString()
+    @IsNotEmpty({ message: "Название пространства обязательно" })
+    name!: string
+
+    @IsEnum(SpaceType, { message: "Неверный тип пространства (DESK, MEETING_ROOM, PHONE_BOOTH)" })
+    type!: SpaceType
+
+    @IsInt()
+    @Min(1, { message: "Вместимость должна быть минимум 1 человек" })
+    @IsOptional()
+    capacity?: number
+    
+    @IsInt()
+    @Min(0)
+    @IsOptional()
+    pricePerHour?: number
+
+    @IsNumber()
+    @IsOptional()
+    mapX?: number
+
+    @IsNumber()
+    @IsOptional()
+    mapY?: number
+
+    @IsBoolean()
+    @IsOptional()
+    isActive?: boolean
+}
