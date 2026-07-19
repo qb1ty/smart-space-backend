2515db3a4c925e5f797be17abdb99cf286362164
qb1ty <arsenbro0707@mail.ru>
2026-07-19 11:36:03 +0500

    upgrade: обновил и добавил новые утилиты

--- src/reports/utils/reports-db.util.ts
+++ src/reports/utils/reports-db.util.ts
@@ -69,3 +69,53 @@ export function getCancelledBookingsCountQuery(
         }
     })
 }
+
+export function getAllSpaces(prisma: PrismaClient) {
+    return prisma.space.findMany({
+        where: { isActive: true },
+        select: { id: true, name: true, type: true, pricePerHour: true },
+        orderBy: { name: "asc" }
+    })
+}
+
+export function getBookingForEfficiencyQuery(prisma: PrismaClient, where: Prisma.BookingWhereInput) {
+    return prisma.booking.findMany({
+        where,
+        select: { spaceId: true, startTime: true, endTime: true, totalCost: true }
+    })
+}
+
+export function getUsersActiveStatsQuery(prisma: PrismaClient, where: Prisma.BookingWhereInput) {
+    return prisma.booking.groupBy({
+        by: ["userId"],
+        _count: { id: true },
+        _sum: { totalCost: true },
+        where
+    })
+}
+
+export function getUsersCancelledStatsQuery(prisma: PrismaClient, from?: string, to?: string) {
+    const dateFilter: Prisma.DateTimeFilter = {}
+    if (from) {
+        dateFilter.gte = new Date(from)
+    }
+
+    if (to) {
+        dateFilter.lte = new Date(to)
+    }
+
+    return prisma.booking.groupBy({
+        by: ["userId"],
+        _count: { id: true },
+        where: {
+            status: BookingStatus.CANCELLED,
+            ...(from || to ? { startTime: dateFilter } : {})
+        }
+    })
+}
+
+export function getAllUsersDictQuery(prisma: PrismaClient) {
+    return prisma.user.findMany({
+        select: { id: true, firstname: true, lastname: true, email: true }
+    })
+}
